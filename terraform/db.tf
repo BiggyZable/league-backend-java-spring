264fbfebaa9a0e50b6a81edd009359e5464741e2
bencenguyen <bence325@gmail.com>
2021-08-18 15:26:25 +0200

    fix: db name and user

--- terraform/db.tf
+++ terraform/db.tf
@@ -1,11 +1,11 @@
 resource "aws_db_instance" "league-db" {
   identifier             = "league-db"
-  name                   = "league-db"
+  name                   = "leaguedb"
   instance_class         = "db.t3.micro"
   allocated_storage      = 5
   engine                 = "postgres"
   engine_version         = "13.1"
-  username               = "admin"
+  username               = "leagueadmin"
   password               = var.db_password
   db_subnet_group_name   = aws_db_subnet_group.league-subnetgroup.name
   vpc_security_group_ids = [aws_security_group.league-sg.id]
